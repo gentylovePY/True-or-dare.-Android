e30a37cf09ea263612fe842bb155ccb7bf4f4a15
gentylovePY <50623329+gentylovePY@users.noreply.github.com>
2020-04-29 17:54:44 +0300

    1day True

---
+++ app/src/main/java/com/example/trueordare/Array.java
@@ -0,0 +1,29 @@
+package com.example.trueordare;
+
+public class Array {
+    final int[] True= {
+            R.string.one,
+            R.string.two,
+            R.string.three,
+            R.string.four,
+            R.string.five,
+            R.string.six,
+            R.string.seven,
+            R.string.eight,
+            R.string.nine,
+            R.string.ten,
+
+    };
+    final int[]Dare = {
+            R.string.oneDare,
+            R.string.twoDare,
+            R.string.threeDare,
+            R.string.fourDare,
+            R.string.fiveDare,
+            R.string.sixDare,
+            R.string.sevenDare,
+            R.string.eightDare,
+            R.string.nineDare,
+            R.string.tenDare,
+    };
+}
--- app/src/main/java/com/example/trueordare/MainActivity.java
+++ app/src/main/java/com/example/trueordare/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.trueordare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Dialog dialog;
+    Button True,Date;
+    int num;
+    Random randrom = new Random();
+    TextView textView_description_end;
+    Array array = new Array();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setContentView(R.layout.previewdialog_end);
+
+
+        True = findViewById(R.id.True);
+        Date = findViewById(R.id.dare);
+
+
+        textView_description_end = dialog.findViewById(R.id.textView_description_end);
+
+
+
+
+
+
+        True.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                num= randrom.nextInt(10);
+                textView_description_end.setText(array.True[num]);
+                dialog.show();
+                return false;
+            }
+        });
+
+
     }
 }
